f363ea8f5824a361a2599f97b624533a47f308b3
GpAndrad2 <123668518+GpAndrad2@users.noreply.github.com>
2024-05-20 21:56:17 -0300

    Add files via upload

--- JS/buscar-encomenda.js
+++ JS/buscar-encomenda.js
@@ -2,11 +2,17 @@ var botaoBuscar = document.querySelector("#buscar-encomenda");
 
 botaoBuscar.addEventListener("click", function(){
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "htts://localhost:3000/encomendas");
+  xhr.open("GET", "htts://localhost:3000/encomenda");
   xhr.addEventListener("load", function(){
     var resposta = xhr .responseText;
     console.log(resposta);
     console.log(typeof resposta0);
+        var resposta = xhr.responseText;
+        var encomenda = JSON.parse(resposta);
+
+    encomenda.forEach(function(cada_encomenda){
+        addEncomenda(cada_encomenda);
+    })
   });
     xhr.send();
 })
